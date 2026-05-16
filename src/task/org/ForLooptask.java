470458ec642cd803bdc622670b8553185844470a
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-05-16 22:13:47 +0530

    ForLoop

--- src/task/org/ForLooptask.java
+++ src/task/org/ForLooptask.java
@@ -1,33 +1,68 @@
 package task.org;
 
-public class ForLooptask {
+public class ForloopTask {
 	
 	public void numbers () {
-		for (int i=1; i<=10 ;i++) {
+		
+		for(int i=1; i<=10; i++) {
 			System.out.println(i);
-		}	
+			
+		}
+		
 	}
 	
-	public void reverse () {
-		for (int i=10; i>=0; i--) {
+	public void evenNmuber() {
+		for(int i=1; i<=30; i++) {
+			if(i %2 ==0)
 			System.out.println(i);
+			
 		}
 	}
-	 public void multiplication () {
-		 for (int i=1; i<=10 ; i++) {
-			 System.out.println(i+ "x2=" + i*2);
-		 }
-	 }
 	
-	
-	
-	public static void main(String[] args) {
-		ForLooptask obj = new ForLooptask();
+	public void sumoffivenumbers() {
+		int j=0;
 		
-		obj.numbers();
-		obj.reverse();
-		obj.multiplication();
+		for(int i=1; i<=5; i++) {
+			
+			j=j+i;
+		}
+			System.out.println(j);
+			}
 		
-				
+	public void reverse() {
+		
+		for(int i= 10; i>=1;i--) {
+			System.out.println(i);
+			
+		}
 	}
+	
+	public void multiplication() {
+		
+		for(int i=1; i<=10; i++) {
+		System.out.println(i+" x 2 = "+ (i*2));
+		}
+	}
+	
+	public void factorial() {
+		
+		int j=1;
+		for(int i=5; i>=1; i--) {
+			j=j*i;
+			System.out.println(j);
+		}
+			
+		}
+	
+public static void main(String[] args) {
+	ForloopTask obj = new ForloopTask();
+	//obj.numbers();
+	//obj.evenNmuber();
+	//obj.sumoffivenumbers();
+    //obj.reverse();
+	//obj.multiplication();	
+	//obj.factorial();
+	
+	
+}
 }
