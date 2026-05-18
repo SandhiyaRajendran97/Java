34a9f838de52466025b0efeded1b0a4483cb571f
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-05-18 15:07:14 +0530

    Looping

---
+++ src/task/org/Looping.java
@@ -0,0 +1,69 @@
+package task.org;
+
+public class Looping {
+	
+	
+	public void numbers () {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+			
+		}
+		
+	}
+	
+	public void evenNmuber() {
+		for(int i=1; i<=30; i++) {
+			if(i %2 ==0)
+			System.out.println(i);
+			
+		}
+	}
+	
+	public void sumoffivenumbers() {
+		int j=0;
+		
+		for(int i=1; i<=5; i++) {
+			
+			j=j+i;
+		}
+			System.out.println(j);
+			}
+		
+	public void reverse() {
+		
+		for(int i= 10; i>=1;i--) {
+			System.out.println(i);
+			
+		}
+	}
+	
+	public void multiplication() {
+		
+		for(int i=1; i<=10; i++) {
+		System.out.println(i+" x 2 = "+ (i*2));
+		}
+	}
+	
+	public void factorial() {
+		
+		int j=1;
+		for(int i=5; i>=1; i--) {
+			j=j*i;
+			System.out.println(j);
+		}
+			
+		}
+	
+public static void main(String[] args) {
+	Looping obj = new Looping();
+	//obj.numbers();
+	//obj.evenNmuber();
+	//obj.sumoffivenumbers();
+    //obj.reverse();
+	//obj.multiplication();	
+	obj.factorial();
+	
+	
+}
+}
