672de6eece0573ea183851ea62fbee26302744f0
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-06-08 23:41:31 +0530

    TaskFour

---
+++ src/variableTask/TaskFour.java
@@ -0,0 +1,24 @@
+package variableTask;
+
+public class TaskFour{
+	
+	public void english(){
+		System.out.println("Pass");
+	}
+	public void tamil(){
+		System.out.println("Pass");
+	}
+	public void maths(){
+		System.out.println("pass");
+	}
+	public void science(){
+		System.out.println("pass");
+	}
+	public void social(){
+		System.out.println("pass");
+	}
+}
+
+
+
+
