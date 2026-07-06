3b5d2843bcef7bcfc6bd8755fdfda3db88126c47
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-07-06 23:28:17 +0530

    TaskCalculator

---
+++ src/task/org/TaskCalculator.java
@@ -0,0 +1,24 @@
+package task.org;
+
+public class TaskCalculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        TaskCalculator obj = new TaskCalculator();
+
+        System.out.println(obj.add(10, 20));       // 30
+        System.out.println(obj.add(10, 20, 30));   // 60
+        System.out.println(obj.add(10.5, 20.5));   // 31.0
+    }
+}
